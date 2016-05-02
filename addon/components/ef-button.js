27381632d7157f1a531299fc74d87ab40866f013
Jeremy Brown <jeremy@notmessenger.com>
2016-05-01 20:15:47 -0500

    Add skeleton of ef-button component

---
+++ addon/components/ef-button.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import layout from '../templates/components/ef-button';
+
+/**
+ * @module
+ * @augments ember/Component
+ */
+export default Ember.Component.extend({
+
+});
