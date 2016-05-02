c0299e3536e292af9aee32b87693f2ccaa145661
Jeremy Brown <jeremy@notmessenger.com>
2016-05-02 07:12:54 -0500

    Closes ember-forge/ember-forge-ui#8, Closes ember-forge/ember-forge-ui#9

--- blueprints/ember-forge-ui/index.js
+++ blueprints/ember-forge-ui/index.js
@@ -2,12 +2,10 @@
 
 module.exports = {
     afterInstall: function() {
-        return this.addAddonsToProject({
-            packages: [
-                { name: 'broccoli-file-creator', target: '~1.1.0' },
-                { name: 'broccoli-merge-trees', target: '~1.1.1' }
-            ]
-        });
+        return this.addPackagesToProject([
+          { name: 'broccoli-file-creator', target: '~1.1.0' },
+          { name: 'broccoli-merge-trees', target: '~1.1.1' }
+        ]);
     },
 
     normalizeEntityName: function() {}
