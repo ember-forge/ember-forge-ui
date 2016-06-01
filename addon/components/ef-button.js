7b90eea2731bac2354741b20652bba6a4118d90a
Jeremy Brown <jeremy@notmessenger.com>
2016-06-01 10:35:33 -0500

    Temporarily add logging of all event hooks

--- addon/components/ef-button.js
+++ addon/components/ef-button.js
@@ -36,6 +36,106 @@ export default Component.extend( AttributeBindings, ClassNames, ClassNameBinding
     // Events
 
     /**
+     * didInsertElement event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    didInsertElement() {
+        console.log( 'didInsertElement() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * didReceiveAttrs event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    didReceiveAttrs() {
+        console.log( 'didReceiveAttrs() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * didRender event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    didRender() {
+        console.log( 'didRender() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * didUpdate event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    didUpdate() {
+        console.log( 'didUpdate() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * didUpdateAttrs event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    didUpdateAttrs() {
+        console.log( 'didUpdateAttrs() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * willClearRender event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    willClearRender() {
+        console.log( 'willClearRender() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * willDestroyElement event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    willDestroyElement() {
+        console.log( 'willDestroyElement() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * willInsertElement event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    willInsertElement() {
+        console.log( 'willInsertElement() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * willRender event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    willRender() {
+        console.log( 'willRender() from ember-forge-ui ef-button' );
+    },
+
+    /**
+     * willUpdate event hook
+     *
+     * @function
+     * @returns {undefined}
+     */
+    willUpdate() {
+        console.log( 'willUpdate() from ember-forge-ui ef-button' );
+    },
+
+    /**
      * init event hook
      *
      * @function
@@ -44,6 +144,8 @@ export default Component.extend( AttributeBindings, ClassNames, ClassNameBinding
     init() {
         this._super( ...arguments );
         this.componentInit();
+
+        console.log( 'init() from ember-forge-ui ef-button' );
     },
 
     // -------------------------------------------------------------------------
