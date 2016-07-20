227940b742e938abf4a3ab86cb7adbe9006aaf6d
Jeremy Brown <jeremy@notmessenger.com>
2016-07-20 06:46:19 -0500

    Implementation of ef-list-item component (#41)

--- addon/components/ef-list-item.js
+++ addon/components/ef-list-item.js
@@ -17,12 +17,23 @@ export default Component.extend({
   // Attributes
 
   /** @type {String[]} */
+  attributeBindings: [
+    'value'
+  ],
+
+  /** @type {String[]} */
+  classNameBindings: [
+    'active:ef-active',
+    'disabled:ef-disabled'
+  ],
+
+  /** @type {String[]} */
   classNames: [
-    'ef-nav-list-item'
+    'ef-list-item'
   ],
 
   /** @type {String} */
-  tagName: 'li'
+  tagName: 'li',
 
   // -------------------------------------------------------------------------
   // Actions
@@ -32,6 +43,28 @@ export default Component.extend({
 
   // -------------------------------------------------------------------------
   // Properties
+
+  /**
+   * Whether represents an active list item
+   *
+   * @type {Boolean}
+   */
+  active: false,
+
+  /**
+   * Whether represents a disabled list item
+   *
+   * @type {Boolean}
+   */
+  disabled: false,
+
+  /**
+   * Ordinal value of the list item
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/semantics.html#the-li-element}
+   * @type {?Number}
+   */
+  value: null
 
   // -------------------------------------------------------------------------
   // Observers
