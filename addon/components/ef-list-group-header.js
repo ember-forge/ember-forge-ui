a543955f4dd6201dcb6700b7209193cc213f1b06
Jeremy Brown <jeremy@notmessenger.com>
2016-07-29 07:19:54 -0500

    Refactor ef-list-group-header component

--- addon/components/ef-list-group-header.js
+++ addon/components/ef-list-group-header.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 const {
-  Component
+  Component,
+  get
 } = Ember;
 
 /**
@@ -16,13 +17,8 @@ export default Component.extend({
   // -------------------------------------------------------------------------
   // Attributes
 
-  /** @type {String[]} */
-  classNames: [
-    'ef-list-group-header'
-  ],
-
   /** @type {String} */
-  tagName: 'li'
+  tagName: 'li',
 
   // -------------------------------------------------------------------------
   // Actions
@@ -30,13 +26,62 @@ export default Component.extend({
   // -------------------------------------------------------------------------
   // Events
 
+  /**
+   * init event hook
+   *
+   * Set context classes
+   *
+   * @function
+   * @returns {undefined}
+   */
+  init() {
+    this._super(...arguments);
+
+    this.setContextClasses();
+  },
+
   // -------------------------------------------------------------------------
   // Properties
+
+  /**
+   * Whether the list items are ordered in the list
+   *
+   * @type {Boolean}
+   */
+  ordered: false,
+
+  /**
+   * The context in which the component is being used
+   *
+   * @type {?String}
+   */
+  usedAs: null,
 
   // -------------------------------------------------------------------------
   // Observers
 
   // -------------------------------------------------------------------------
   // Methods
+
+  /**
+   * Manage class names dependent on context of usage
+   *
+   * @private
+   * @returns {undefined}
+   */
+  setContextClasses() {
+    let className;
+
+    switch(get(this, 'usedAs')) {
+      case 'nav':
+        className = 'ef-nav-list-group-header';
+        break;
+
+      default:
+        className = 'ef-list-group-header';
+    }
+
+    get(this, 'classNames').addObject(className);
+  }
 
 });
