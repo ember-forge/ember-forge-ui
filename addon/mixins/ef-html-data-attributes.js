bd68578ffaa52cb3de77cc5e6e5afeb0efe09825
Jeremy Brown <jeremy@notmessenger.com>
2016-08-08 14:52:42 -0500

    Add data attributes mixin

---
+++ addon/mixins/ef-html-data-attributes.js
@@ -0,0 +1,69 @@
+import Ember from 'ember';
+
+const {
+  get,
+  Mixin
+} = Ember;
+
+/**
+ * Add bindings for custom HTML data attributes
+ *
+ * @module
+ * @augments ember/Mixin
+ * @see {@link https://html.spec.whatwg.org/multipage/dom.html#embedding-custom-non-visible-data-with-the-data-*-attributes}
+ */
+export default Mixin.create({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  /**
+   * init event hook
+   *
+   * Bind provided `data-*` attributes
+   *
+   * @function
+   * @returns {undefined}
+   */
+  init() {
+    this._super(...arguments);
+
+    this.bindAttributes();
+  },
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  /**
+   * Bind provided `data-*` attributes
+   *
+   * @returns {undefined}
+   */
+  bindAttributes() {
+    let bindings = get(this, 'attributeBindings');
+
+    Object.keys(this).forEach(function(element) {
+      if(/^data-./.test(element.toLowerCase())) {
+        bindings.push(element);
+      }
+    });
+
+    this.set('attributeBindings', bindings);
+  }
+
+});
