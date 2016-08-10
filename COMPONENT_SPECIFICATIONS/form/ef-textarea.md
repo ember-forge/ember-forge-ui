81781db14286aa3adc0cef63a552f2aaaf6b23c9
Jeremy Brown <jeremy@notmessenger.com>
2016-08-09 20:11:18 -0500

    Add form/ef-textarea component

---
+++ COMPONENT_SPECIFICATIONS/form/ef-textarea.md
@@ -0,0 +1,69 @@
+# form/ef-textarea
+
+## Description
+
+Render a `textarea` HTML element
+
+
+## Influences
+
+* https://html.spec.whatwg.org/multipage/forms.html#the-textarea-element
+* https://guides.emberjs.com/v2.7.0/templates/input-helpers/#toc_text-areas
+
+
+## Specification
+
+### HTML element
+
+* textarea
+
+
+### HTML Attributes
+
+* autocapitalize
+* autocomplete
+* autofocus
+* cols
+* dirname
+* disabled
+* form
+* inputmode — Hint for selecting an input modality
+* maxlength
+* minlength
+* name
+* placeholder
+* readonly
+* required
+* rows
+* wrap
+* all from ef-html-aria-attributes mixin
+* all from ef-html-global-attributes mixin
+
+
+### CSS Class
+
+* ef-textarea
+
+
+### CSS Class Name Bindings
+
+* None
+
+
+## Properties
+
+**text** *String*
+
+* Text to display when used in non-block form
+
+
+
+### Block/Non Block support
+
+* **Block support:** *Yes*
+* **Non-Block support:** *Yes*
+
+
+## Example Usage
+
+
---
+++ addon/components/form/ef-textarea.js
@@ -0,0 +1,207 @@
+import AriaAttributes from 'ember-forge-ui/mixins/ef-html-aria-attributes';
+import DataAttributes from 'ember-forge-ui/mixins/ef-html-data-attributes';
+import ElementEvents from 'ember-forge-ui/mixins/ef-html-element-events';
+import Ember from 'ember';
+import GlobalAttributes from 'ember-forge-ui/mixins/ef-html-global-attributes';
+import InputSelection from 'ember-forge-ui/mixins/ef-html-input-selection';
+
+const {
+  Component
+} = Ember;
+
+/**
+ * @module
+ * @augments ember/Component
+ * @augments ember-forge-ui/mixins/ef-html-aria-attributes
+ * @augments ember-forge-ui/mixins/ef-html-data-attributes
+ * @augments ember-forge-ui/mixins/ef-html-element-events
+ * @augments ember-forge-ui/mixins/ef-html-global-attributes
+ * @augments ember-forge-ui/mixins/ef-html-input-selection
+ */
+export default Component.extend(AriaAttributes, DataAttributes, ElementEvents, GlobalAttributes, InputSelection, {
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  attributeBindings: [
+    'autocapitalize',
+    'autocomplete',
+    'autofocus',
+    'cols',
+    'dirname',
+    'disabled',
+    'form',
+    'inputmode',
+    'maxlength',
+    'minlength',
+    'name',
+    'placeholder',
+    'readonly',
+    'required',
+    'rows',
+    'wrap'
+  ],
+
+  /** @type {String[]} */
+  classNames: [
+    'ef-textarea'
+  ],
+
+  /** @type {String} */
+  tagName: 'textarea',
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * Specifies the auto-capitalization behavior of text elements
+   *
+   * @see {@link https://developer.apple.com/library/safari/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/Attributes.html}
+   * @type {?String}
+   */
+  autocapitalize: null,
+
+  /**
+   * Hint to the user agent how to, or indeed whether to, provide features for helping users fill forms in
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-autocomplete}
+   * @type {?String}
+   */
+  autocomplete: null,
+
+  /**
+   * Automatically focus the form control when the page is loaded
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-autofocus}
+   * @type {?Boolean}
+   */
+  autofocus: null,
+
+  /**
+   * Maximum number of characters per line
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-cols}
+   * @type {?Number}
+   */
+  cols: null,
+
+  /**
+   * Name of form control to use for sending the element's directionality in form submission
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-dirname}
+   * @type {?String}
+   */
+  dirname: null,
+
+  /**
+   * Whether the form control is disabled
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-disabled}
+   * @type {?Boolean}
+   */
+  disabled: null,
+
+  /**
+   * Associates the control with a form element
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fae-form}
+   * @type {?String}
+   */
+  form: null,
+
+  /**
+   * Specifies what kind of input mechanism would be most helpful for users entering content into the form control.
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-inputmode}
+   * @type {?String}
+   */
+  inputmode: null,
+
+  /**
+   * Maximum length of value
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-maxlength}
+   * @type {?Number}
+   */
+  maxlength: null,
+
+  /**
+   * Minimum length of value
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-minlength}
+   * @type {?Number}
+   */
+  minlength: null,
+
+  /**
+   * Name of form control to use for form submission and in the form.elements API
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-fe-name}
+   * @type {?String}
+   */
+  name: null,
+
+  /**
+   * User-visible label to be placed within the form control
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-placeholder}
+   * @type {?String}
+   */
+  placeholder: null,
+
+  /**
+   * Whether to allow the value to be edited by the user
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-readonly}
+   * @type {?Boolean}
+   */
+  readonly: null,
+
+  /**
+   * Whether the control is required for form submission
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-required}
+   * @type {?Boolean}
+   */
+  required: null,
+
+  /**
+   * Number of lines to show
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-rows}
+   * @type {?Number}
+   */
+  rows: null,
+
+  /**
+   * Text to display when used in non-block form
+   *
+   * @type {?String}
+   */
+  text: null,
+
+  /**
+   * How the value of the form control is to be wrapped for form submission
+   *
+   * @see {@link https://html.spec.whatwg.org/multipage/forms.html#attr-textarea-wrap}
+   * @type {?String}
+   */
+  wrap: null
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+});
