69b276ec08d3cfa8ad40379aae978bd0bee45d04
Jeremy Brown <jeremy@notmessenger.com>
2016-08-13 22:19:40 -0500

    Remove setting of `tagName` to `''`
    See https://github.com/emberjs/ember.js/issues/14023 for more information

--- addon/components/ef-list.js
+++ addon/components/ef-list.js
@@ -25,9 +25,6 @@ export default Component.extend({
     'type'
   ],
 
-  /** @type {String} */
-  tagName: '',
-
   // -------------------------------------------------------------------------
   // Actions
 
