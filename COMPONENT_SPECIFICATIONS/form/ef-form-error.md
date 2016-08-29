78df907dde1c7ef8f59ce5d70d2527146e4b6aa3
Jeremy Brown <jeremy@notmessenger.com>
2016-08-29 08:41:35 -0500

    form/ef-form-error component

---
+++ COMPONENT_SPECIFICATIONS/form/ef-form-error.md
@@ -0,0 +1,113 @@
+# form/ef-form-error
+
+## Description
+
+Display error messages for properties that are not being managed by individual `form/ef-element-error` instances, including the ability to be a "catch" all error output for errors which weren't anticipated (on a field-by-field, as well as form-wide basis).
+
+
+
+## Influences
+
+* http://parsleyjs.org/doc/examples.html
+* https://hyperform.js.org
+  * Complete implementation of the HTML 5 form validation API in Javascript
+* https://github.com/davewasmer/ember-cli-formtastic/blob/master/addon/components/input-for.js
+* https://github.com/davewasmer/ember-cli-formtastic/blob/master/addon/components/error-for.js
+* https://github.com/nibynic/ember-form-builder
+* https://github.com/martndemus/ember-form-for/blob/25309b3f248133f4b2e454618dbdf640d1aa8bbd/addon/components/form-field.js#L106
+
+
+## Specification
+
+### HTML element
+
+* div
+
+
+### HTML Attributes
+
+* None
+
+
+### CSS Class
+
+* ef-form-error
+
+
+### CSS Class Name Bindings
+
+These are CSS class name bindings, listed as **property:** *class name applied* (default state)
+
+* **hasMessages:** *ef-form-error-hasMessages* (defaults to `hasMessages: false`)
+
+
+## Properties
+
+* None
+
+
+
+### Block/Non Block support
+
+* **Block support:** *Yes*
+* **Non-Block support:** *Yes*
+
+
+## Example Usage
+
+```
+// template
+
+{{#ef-form
+  data=theData
+  errors=theErrors
+  as |f|
+}}
+  {{f.form-error}}
+{{/ef-form}}
+
+// rendered HTML
+
+<form class="ef-form">
+  <div class="ef-form-error ef-form-error-hasMessages">
+    <ul class="ef-list">
+      <li class="ef-list-item">
+        ...
+      </li>
+      <li class="ef-list-item">
+        ...
+      </li>
+    </ul>
+  </div>
+</form>
+
+<form class="ef-form">
+  <div class="ef-form-error"></div>
+</form>
+```
+
+```
+// template
+
+{{#ef-form
+  data=theData
+  errors=theErrors
+  as |f|
+}}
+  {{#f.form-error as |messages|}}
+    {{#each messages as |message|}}
+      <p>{{message}}</p>
+    {{/each}}
+  {{/f.form-error}}
+{{/ef-form}}
+
+// rendered HTML
+
+<form class="ef-form">
+  <div class="ef-form-error ef-form-error-hasMessages">
+    <p>...</p>
+    <p>...</p>
+  </div>
+</form>
+```
+
---
+++ addon/components/form/ef-form-error.js
@@ -0,0 +1,170 @@
+import Ember from 'ember';
+
+const {
+  Component,
+  computed,
+  get,
+  isEmpty,
+  set
+} = Ember;
+
+/**
+ * @module
+ * @augments ember/Component
+ */
+export default Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  /** @type {String[]} */
+  classNameBindings: [
+    'hasMessages:ef-form-error-hasMessages'
+  ],
+
+  /** @type {String[]} */
+  classNames: [
+    'ef-form-error'
+  ],
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  /**
+   * Initialize messages array
+   *
+   * @returns {undefined}
+   */
+  init() {
+    this._super(...arguments);
+
+    set(this, 'allErrorProperties', Object.keys(get(this, 'errors')));
+    set(this, 'messages', Ember.A());
+  },
+
+  /**
+   * Add observers for dynamic properties
+   *
+   * @returns {undefined}
+   */
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.addObservers();
+  },
+
+  /**
+   * Remove observers for dynamic properties
+   *
+   * @returns {undefined}
+   */
+  willDestroyElement() {
+    this._super(...arguments);
+
+    this.removeObservers();
+  },
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * All error properties that exist on the error object
+   *
+   * @type {?Array}
+   */
+  allErrorProperties: null,
+
+  /**
+   * Error properties that are not being managed by individual `form/ef-element-error` instances
+   *
+   * @type {?Array}
+   */
+  availableErrors: null,
+
+  /**
+   * Whether there are any error messages to display
+   *
+   * @type {ember/Computed/notEmpty}
+   */
+  hasMessages: computed.notEmpty('messages'),
+
+  /**
+   * Error messages for properties that are not being managed by individual `form/ef-element-error` instances
+   *
+   * @type {?ember/Array}
+   */
+  messages: null,
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  /**
+   * Add observers to dynamic properties
+   *
+   * @returns {undefined}
+   */
+  addObservers() {
+    const allErrorProperties = new Set(get(this, 'allErrorProperties'));
+    const registeredProperties = new Set(get(this, 'registeredErrors'));
+    const propertiesNotManaged = new Set(
+      [...allErrorProperties].filter(property => !registeredProperties.has(property))
+    );
+
+    set(this, 'availableErrors', [...propertiesNotManaged]);
+
+    this.addObserver('registeredErrors.[]', this, 'updateMessages');
+
+    get(this, 'availableErrors').forEach((value) => {
+      this.addObserver(`errors.${value}`, this, 'updateMessages');
+    });
+  },
+
+  /**
+   * Remove observers to dynamic properties
+   *
+   * @returns {undefined}
+   */
+  removeObservers() {
+    this.removeObserver('registeredErrors.[]', this, 'updateMessages');
+
+    get(this, 'availableErrors').forEach((value) => {
+      this.removeObserver(`errors.${value}`, this, 'updateMessages');
+    });
+  },
+
+  /**
+   * Update messages array
+   *
+   * @returns {undefined}
+   */
+  updateMessages() {
+    let messages = [];
+
+    get(this, 'availableErrors').forEach((property) => {
+      let theMessage = get(this, `errors.${property}`);
+
+      if (!isEmpty(theMessage)) {
+        if (Array.isArray(theMessage)) {
+          theMessage.forEach(function(message) {
+            messages.push(message);
+          });
+
+        } else {
+          messages.push(theMessage);
+        }
+      }
+    });
+
+    set(this, 'messages', messages);
+  }
+
+});
