5e9f7ddb86ff196f98176be876f0cab1cf45702b
Jeremy Brown <jeremy@notmessenger.com>
2016-09-13 10:26:19 -0500

    Refactor lifecyle hook listening to

--- addon/components/form/ef-form-error.js
+++ addon/components/form/ef-form-error.js
@@ -59,7 +59,7 @@ export default Component.extend({
   didInsertElement() {
     this._super(...arguments);
 
-    this.addObservers();
+    this.addDynamicObservers();
   },
 
   /**
@@ -70,7 +70,7 @@ export default Component.extend({
   willDestroyElement() {
     this._super(...arguments);
 
-    this.removeObservers();
+    this.removeDynamicObservers();
   },
 
   // -------------------------------------------------------------------------
@@ -129,20 +129,13 @@ export default Component.extend({
    *
    * @returns {undefined}
    */
-  addObservers() {
-    const allErrorProperties = new Set(get(this, 'allErrorProperties'));
-    const registeredProperties = new Set(get(this, 'registeredErrors'));
-    const propertiesNotManaged = new Set(
-      [...allErrorProperties].filter(property => !registeredProperties.has(property))
-    );
+  addDynamicObservers() {
 
-    set(this, 'availableErrors', [...propertiesNotManaged]);
+// react to "registeredErrors" changes by calling "updateAvailableErrors()" (which updates "availableErrors")
+    addObserver(this, 'registeredErrors.[]', this, 'updateAvailableErrors');
 
-    addObserver(this, 'registeredErrors.[]', this, 'updateMessages');
-
-    get(this, 'availableErrors').forEach((value) => {
-      addObserver(this, `errors.${value}`, this, 'updateMessages');
-    });
+// react to "availableErrors" changes by removing previous observers and adding new ones (that call "updateMessages")
+    addObserver(this, 'availableErrors.[]', this, 'updateDynamicObservers');
   },
 
   /**
@@ -150,11 +143,27 @@ export default Component.extend({
    *
    * @returns {undefined}
    */
-  removeObservers() {
+  removeDynamicObservers() {
     removeObserver(this, 'registeredErrors.[]', this, 'updateMessages');
 
     get(this, 'availableErrors').forEach((value) => {
       removeObserver(this, `errors.${value}`, this, 'updateMessages');
+    });
+  },
+
+  /**
+   * Update observers to dynamic properties
+   *
+   * @returns {undefined}
+   */
+  updateDynamicObservers() {
+console.warn('this needs to be against a previously-known list, not the new one already updated');
+    get(this, 'availableErrors').forEach((value) => {
+      removeObserver(this, `errors.${value}`, this, 'updateMessages');
+    });
+
+    get(this, 'availableErrors').forEach((value) => {
+      addObserver(this, `errors.${value}`, this, 'updateMessages');
     });
   },
 
@@ -170,6 +179,16 @@ export default Component.extend({
     });
   },
 
+  updateAvailableErrors() {
+    const allErrorProperties = new Set(get(this, 'allErrorProperties'));
+    const registeredProperties = new Set(get(this, 'registeredErrors'));
+    const propertiesNotManaged = new Set(
+      [...allErrorProperties].filter(property => !registeredProperties.has(property))
+    );
+
+    set(this, 'availableErrors', [...propertiesNotManaged]);
+  },
+
   /**
    * Update messages array
    *
@@ -177,7 +196,8 @@ export default Component.extend({
    */
   updateMessages() {
     let messages = [];
-
+console.info('ef-form-error updateMessages()');
+console.log('availableErrors', get(this, 'availableErrors'));
     get(this, 'availableErrors').forEach((property) => {
       let theMessage = get(this, `errors.${property}`);
 
